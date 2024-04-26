0225298590e74f5d17440ef6e417fb073863fb57
maaouikarima <maaouika07@gmail.com>
2024-04-26 09:43:51 +0100

    Initial Commit

---
+++ movieApp/movieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieAppApp.swift
+//  movieApp
+//
+//  Created by Tekup-mac-3 on 26/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct movieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
