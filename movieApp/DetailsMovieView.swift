a689f9bed030d7044a09cd80a9a22b2de5d1e61c
maaouikarima <maaouika07@gmail.com>
2024-05-03 13:45:05 +0100

    last commit

---
+++ movieApp/DetailsMovieView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsMovieView.swift
+//  movieApp
+//
+//  Created by Tekup-mac-3 on 3/5/2024.
+//
+
+import SwiftUI
+
+struct DetailsMovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsMovieView()
+}
---
+++ movieApp/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  movieApp
+//
+//  Created by Tekup-mac-3 on 3/5/2024.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
