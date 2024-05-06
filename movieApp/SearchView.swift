62f470e3ff7917a8cc4caac952b1548e22129bd2
karimamaaoui <67017326+karimamaaoui@users.noreply.github.com>
2024-05-06 13:54:44 +0100

    Update SearchView.swift

--- movieApp/SearchView.swift
+++ movieApp/SearchView.swift
@@ -5,11 +5,18 @@
 //  Created by Tekup-mac-3 on 3/5/2024.
 //
 
+
 import SwiftUI
 
 struct SearchView: View {
+    @State var txt=""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack (spacing: 15) {
+            Image("magnifyingglass").font(.body)
+            TextField("Search Movie",text: $txt)
+        }.padding()
+            .foregroundColor(.black)
+            .background(.blueSecond).cornerRadius(20)
     }
 }
 
