3a66d9a02464d7f4ac867e9d4e280b3ea42dad41
karimamaaoui <67017326+karimamaaoui@users.noreply.github.com>
2024-05-06 13:52:20 +0100

    Update SplashScreenView.swift

--- movieApp/SplashScreenView.swift
+++ movieApp/SplashScreenView.swift
@@ -6,45 +6,42 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct SplashScreenView: View {
     var body: some View {
-                NavigationView {
-                    VStack {
-                        Image("work-from-home")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 370, height: 300)
-                            .padding(.top,24)
-                        Spacer()
-                        Text("Discover Lorem Ipsum")
-                            .font(.system(size: 35 , weight:.bold))
-                            .multilineTextAlignment(.center).foregroundColor(Color("primaryBlue")).padding(.bottom, 8)
-                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry.").font(.system(size: 14 , weight:.regular)).multilineTextAlignment(.center).foregroundColor(.black)
-                            
-                        Spacer()
-                        HStack (spacing: 12){
-                           
-                             NavigationLink(destination: ContentView()){
-                            
-                                 Text("Start").font(.system(size: 20, weight: .semibold)).foregroundColor(.white)
-                            }
-                                .frame(width: 160, height: 60).background(Color("primaryBlue")).cornerRadius(12)
-                            
-                            .frame(width: 160, height: 60)
-                        }
-                        
-                        Spacer()
-                        
-                        }
-                    .padding()
-                    .navigationBarHidden(true)
-                    
+        NavigationView {
+            ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
                 
-                                
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        LottieView(animationFileName: "cinema", loopMode: .loop)
+                            .frame(width: 270, height: 200)
+                        .padding(.top, 24)
+                    }
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: ContentView()) {
+                        Text("Start")
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundColor(.white)
+                            .frame(width: 160, height: 60)
+                            .background(Color.red)
+                            .cornerRadius(12)
+                    }
+                    
+                    Spacer()
                 }
+                .padding()
             }
+            .navigationBarHidden(true)
         }
+    }
+}
 
 #Preview {
     SplashScreenView()
