5fda6b8f59de877fc73e636b869ccefe30724709
karimamaaoui <67017326+karimamaaoui@users.noreply.github.com>
2024-05-06 13:49:35 +0100

    Update DetailsMovieView.swift

--- movieApp/DetailsMovieView.swift
+++ movieApp/DetailsMovieView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct DetailsMovieView: View {
+    let movie: Movie
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(movie.title)
+                .font(.title)
+                .padding()
+            
+            if let posterPath = movie.posterPath {
+                AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 200 , height: 300)
+            } else {
+                Image(systemName: "photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200 , height: 100 )
+            }
+            
+            Text(movie.overview)
+                .padding()
+            
+            Spacer()
+        }
+        .navigationTitle(movie.title)
     }
 }
 
+
+/*
 #Preview {
     DetailsMovieView()
 }
+**/
