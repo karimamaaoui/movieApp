d88987f1e75b0e7a04386f24fb2b32251d946f93
karimamaaoui <67017326+karimamaaoui@users.noreply.github.com>
2024-05-06 13:55:03 +0100

    Update LottieView.swift

--- movieApp/LottieView.swift
+++ movieApp/LottieView.swift
@@ -5,14 +5,25 @@
 //  Created by Tekup-mac-3 on 3/5/2024.
 //
 
+import Lottie
 import SwiftUI
 
-struct LottieView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct LottieView: UIViewRepresentable {
+    
+    var animationFileName: String
+    let loopMode: LottieLoopMode
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
     }
-}
+    
+    func makeUIView(context: Context) -> Lottie.LottieAnimationView {
+        let animationView = LottieAnimationView(name: animationFileName)
+        animationView.loopMode = loopMode
+        animationView.play()
+        animationView.contentMode = .scaleAspectFill
+        animationView.frame = CGRect(x: 0, y: 0, width: 200, height: 200) 
+        return animationView
+    }
 
-#Preview {
-    LottieView()
 }
